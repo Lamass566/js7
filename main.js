c6b759d5cb56974a1081284df906ad4feba3f8bb
Misha sysoiev <vsisoev@ukr.net>
2023-04-07 18:20:04 +0300

    f res3

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ let arr = ['hello', 'world', 23, '23', null];
 let arr2 = [];
 let type = 'string';
 
-function filterBy(array, dataType){
-  array.forEach(function(item){
-    if(typeof(item) != typeof(dataType))
-    arr2.push(item)
-  })
-}
 
-filterBy(arr, type);
+
+function filterBy(array){
+    return typeof(array) != type;
+}
+arr2.push(arr.filter(filterBy));
 console.log(arr2);
